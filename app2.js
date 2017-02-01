8d212067ae1d6dfaa3b54bf39b7afe63a5dc2cbb
Faras Maroge <Goldencrane33@yahoo.com>
2017-02-01 17:12:58 -0500

    change background color to orange

--- app2.js
+++ app2.js
@@ -1,33 +1,33 @@
 //- first define the main parts of the JS code
 //function to add the item, add the items
-function addItem()
-//get the value of the input box
-var itemValue = $('#shopping-list-entry').val();
+function addItem() {
+    //get the value of the input box
+    var itemValue = $('#shopping-list-entry').val();
 
-//validate
-if (itemValue.length === 0) {
-    alert('value is empty');
-} else {
-    //dynamicaly create one row inside the shopping list
-    var row = '';
-    row += '<li>';
-    row += '<span class="shopping-item">' + itemValue + '</span>';
-    row += '<div class="shopping-item-controls">';
-    row += '<button class="shopping-item-toggle">';
-    row += '<span class="button-label">check</span>';
-    row += '</button>';
-    row += '<button class="shopping-item-delete">';
-    row += '<span class="button-label">delete</span>';
-    row += '</button>';
-    row += '</div>';
-    row += '</li>';
+    //validate
+    if (itemValue.length === 0) {
+        alert('value is empty');
+    } else {
+        //dynamicaly create one row inside the shopping list
+        var row = '';
+        row += '<li>';
+        row += '<span class="shopping-item">' + itemValue + '</span>';
+        row += '<div class="shopping-item-controls">';
+        row += '<button class="shopping-item-toggle">';
+        row += '<span class="button-label">check</span>';
+        row += '</button>';
+        row += '<button class="shopping-item-delete">';
+        row += '<span class="button-label">delete</span>';
+        row += '</button>';
+        row += '</div>';
+        row += '</li>';
 
-    //add each row
-    $('.shopping-list').append(row);
+        //add each row
+        $('.shopping-list').append(row);
 
-    //empty the input box
-    $('#shopping-list-entry').val('');
-}
+        //empty the input box
+        $('#shopping-list-entry').val('');
+    }
 }
 
 //function to delete item, delete the items
@@ -40,8 +40,6 @@ function deleteItem() {
 function checkItem() {
     //add the right action
     $(this).parent().parent().toggleClass('shopping-item__checked');
-
-
 }
 /*
 
@@ -49,8 +47,8 @@ Milestone two: start complete the functions definitions and test them line by li
 - inside each functions write in plain english what are the steps to follow
 -complete one step at a time and test it[11/24/16, 9:28:59 PM] Marius Banea: /* Debugging strategies
 $(document).ready(function () {
-    /*on click on the "#js-shopping-list-form button" button activate function called addItem()*/
-* * //
+    /*on click on the "#js-shopping-list-form button" button activate function called addItem()
+*/
 
 
 $(document).ready(function () {
